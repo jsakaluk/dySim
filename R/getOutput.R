4b9e4a4aa31209502841635010b213255ac7daaa
John Sakaluk <johnsakaluk@Johns-MacBook-Pro.local>
2023-02-02 20:34:53 -0500

    feat: add sampMod = "MLM" option for runDySim() and getParamsMLM() helper with help from James Kim and @samanthajoel

--- R/getOutput.R
+++ R/getOutput.R
@@ -27,6 +27,69 @@ getParams <- function(fit, first = NULL, sampCount = NULL){
   return(params)
 }
 
+getParamsMLM <- function(fit, first = NULL, sampCount = NULL){
+
+  apim_dis_A_out <- summary(fit$apim_dis_A)
+  apim_dis_A_conf <- confint(fit$apim_dis_A)
+
+  a1.est <- apim_dis_A_out$tTable[3,1]
+  a1.se <- apim_dis_A_out$tTable[3,2]
+  a1.test <- apim_dis_A_out$tTable[3,3]
+  a1.p <- apim_dis_A_out$tTable[3,4]
+  a1.upper <- apim_dis_A_conf[3,2]
+  a1.lower <- apim_dis_A_conf[3,1]
+
+  a2.est <- apim_dis_A_out$tTable[2,1]
+  a2.se <- apim_dis_A_out$tTable[2,2]
+  a2.test <- apim_dis_A_out$tTable[2,3]
+  a2.p <- apim_dis_A_out$tTable[2,4]
+  a2.upper <- apim_dis_A_conf[2,2]
+  a2.lower <- apim_dis_A_conf[2,1]
+
+  apim_dis_B_out <- summary(fit$apim_dis_B)
+  apim_dis_B_conf <- confint(fit$apim_dis_B)
+
+  p1.est <- apim_dis_B_out$tTable[3,1]
+  p1.se <- apim_dis_B_out$tTable[3,2]
+  p1.test <- apim_dis_B_out$tTable[3,3]
+  p1.p <- apim_dis_B_out$tTable[3,4]
+  p1.upper <- apim_dis_B_conf[3,2]
+  p1.lower <- apim_dis_B_conf[3,1]
+
+  p2.est <- apim_dis_B_out$tTable[2,1]
+  p2.se <- apim_dis_B_out$tTable[2,2]
+  p2.test <- apim_dis_B_out$tTable[2,3]
+  p2.p <- apim_dis_B_out$tTable[2,4]
+  p2.upper <- apim_dis_B_conf[2,2]
+  p2.lower <- apim_dis_B_conf[2,1]
+
+  k1.est <- p1.est/a1.est
+  k2.est <- p2.est/a2.est
+
+  lhs <- c("Y_A", "Y_B", "Y_A", "Y_B", "k1", "k2")
+  op <- c("~", "~", "~", "~", ":=", ":=")
+  rhs <- c("X_A", "X_B", "X_B", "X_A", "p1/a1", "p2/a2")
+  label <- c("a1", "a2", "p1", "p2", "k1", "k2")
+  est <- c(a1.est, a2.est, p1.est, p2.est, k1.est, k2.est)
+  se <- c(a1.se, a2.se, p1.se, p2.se, NA, NA)
+  z <- c(a1.test, a2.test, p1.test, p2.test, NA, NA)
+  pvalue <- c(a1.p, a2.p, p1.p, p2.p, NA, NA)
+  ci.lower <- c(a1.lower, a2.lower, p1.lower, p2.lower, NA, NA)
+  ci.upper <- c(a1.upper, a2.upper, p1.upper, p2.upper, NA, NA)
+
+  mlm.cols <- cbind(lhs, op, rhs, label, est, se, z, pvalue, ci.lower, ci.upper)
+  mlm.df <- as.data.frame(mlm.cols)
+
+  if(isTRUE(first)){
+    params <- mlm.df %>%
+      dplyr::mutate(sim_num = 1)
+  }else if(!isTRUE(first)){
+    sampCount <- sampCount
+    params <- mlm.df %>%
+      dplyr::mutate(sim_num = sampCount)
+  }
+  return(params)
+}
 
 #Function contributions from Omar Camanto (@omarjcamanto)
 getFit <- function(fit, first = NULL, sampCount = NULL){
--- R/runDySim.R
+++ R/runDySim.R
@@ -111,7 +111,6 @@ runDySim <- function(seed = NULL,
   }
   else if(sampMod == "O-APIM"){
     samp <- samp %>%
-      #dplyr::rowwise() %>%
       dplyr::mutate(X_A = rowMeans(dplyr::across(dplyr::starts_with("X_A"))),
                     X_B = rowMeans(dplyr::across(dplyr::starts_with("X_B"))),
                     Y_A = rowMeans(dplyr::across(dplyr::starts_with("Y_A"))),
@@ -174,6 +173,140 @@ runDySim <- function(seed = NULL,
         dplyr::arrange(.data$sim_num)
     }
 
+  }
+  else if(sampMod == "MLM"){
+
+    samp <- samp %>%
+      dplyr::mutate(ID = dplyr::row_number(),
+                    X_A = rowMeans(dplyr::across(dplyr::starts_with("X_A"))),
+                    X_B = rowMeans(dplyr::across(dplyr::starts_with("X_B"))),
+                    Y_A = rowMeans(dplyr::across(dplyr::starts_with("Y_A"))),
+                    Y_B = rowMeans(dplyr::across(dplyr::starts_with("Y_B")))) %>%
+      dplyr::select(ID, X_A, X_B, Y_A, Y_B)
+
+    #Thanks to Sam Joel (@samanthajoel) for the data cleaning pipeline below
+    ##Switch to long format (each participant gets a row)
+    datalong <- stats::reshape(samp,varying=c("X_A","X_B","Y_A","Y_B"),
+                               direction="long", idvar="ID", timevar = "Partner", sep="_")
+
+
+    ##Add partner variables back in
+    dataAPIM <- datalong %>%
+      dplyr::arrange(ID,Partner) %>%
+      dplyr::group_by(ID) %>%
+      dplyr::mutate(X_Actor =  dplyr::case_when(Partner=="A" ~  dplyr::first(X),
+                                                TRUE ~  dplyr::last(X)),
+                    X_Partner =  dplyr::case_when(Partner=="B" ~  dplyr::first(X),
+                                                  TRUE ~  dplyr::last(X)),
+                    Y_Actor =  dplyr::case_when(Partner=="A" ~  dplyr::first(Y),
+                                                TRUE ~  dplyr::last(Y)),
+                    Y_Partner =  dplyr::case_when(Partner=="B" ~  dplyr::first(Y),
+                                                  TRUE ~  dplyr::last(Y)),
+                    Dummy_A = dplyr::case_when(Partner=="A" ~ 1,
+                                               TRUE ~ 0),
+                    Dummy_B = dplyr::case_when(Partner=="B" ~ 1,
+                                               TRUE ~ 0))
+
+    simCount <- 1
+    #Thanks to James Kim for distinguishable APIM nlme::gls code
+    apim_dis_A <- nlme::gls(Y_Actor ~ X_Actor + X_Partner + Dummy_A +
+                              X_Actor*Dummy_A + X_Partner*Dummy_A,
+                            data = dataAPIM,
+                            method = "ML",
+                            correlation = corCompSymm (form=~1|ID),
+                            weights = varIdent(form=~1|Dummy_A),
+                            na.action = na.omit)
+
+    apim_dis_B <- nlme::gls(Y_Actor ~ X_Actor + X_Partner + Dummy_B +
+                              X_Actor*Dummy_B + X_Partner*Dummy_B,
+                            data = dataAPIM,
+                            method = "ML",
+                            correlation = corCompSymm (form=~1|ID),
+                            weights = varIdent(form=~1|Dummy_B),
+                            na.action = na.omit)
+
+    sample.out <- list(apim_dis_A = apim_dis_A,
+                apim_dis_B = apim_dis_B)
+
+    #get output from first sample
+    if(output == "paramTable"){
+      sim.dat <- getParamsMLM(fit = sample.out, sampCount = simCount, first = TRUE)
+    }
+
+    if(nSims > 1){
+      #nSims determines number of loops
+      for(i in cli::cli_progress_along(1:nSimsLess1, name = paste("Simulating", counter, "samples of", sampSize, "dyads."))){
+
+        samp <- simsem::generate(popModScript, n = sampSize)
+
+        samp <- samp %>%
+          dplyr::mutate(ID = dplyr::row_number(),
+                        X_A = rowMeans(dplyr::across(dplyr::starts_with("X_A"))),
+                        X_B = rowMeans(dplyr::across(dplyr::starts_with("X_B"))),
+                        Y_A = rowMeans(dplyr::across(dplyr::starts_with("Y_A"))),
+                        Y_B = rowMeans(dplyr::across(dplyr::starts_with("Y_B")))) %>%
+          dplyr::select(ID, X_A, X_B, Y_A, Y_B)
+
+        #Thanks to Sam Joel (@samanthajoel) for the data cleaning pipeline below
+        ##Switch to long format (each participant gets a row)
+        datalong <- stats::reshape(samp,varying=c("X_A","X_B","Y_A","Y_B"),
+                                   direction="long", idvar="ID", timevar = "Partner", sep="_")
+
+
+        ##Add partner variables back in
+        dataAPIM <- datalong %>%
+          dplyr::arrange(ID,Partner) %>%
+          dplyr::group_by(ID) %>%
+          dplyr::mutate(X_Actor =  dplyr::case_when(Partner=="A" ~  dplyr::first(X),
+                                                    TRUE ~  dplyr::last(X)),
+                        X_Partner =  dplyr::case_when(Partner=="B" ~  dplyr::first(X),
+                                                      TRUE ~  dplyr::last(X)),
+                        Y_Actor =  dplyr::case_when(Partner=="A" ~  dplyr::first(Y),
+                                                    TRUE ~  dplyr::last(Y)),
+                        Y_Partner =  dplyr::case_when(Partner=="B" ~  dplyr::first(Y),
+                                                      TRUE ~  dplyr::last(Y)),
+                        Dummy_A = dplyr::case_when(Partner=="A" ~ 1,
+                                                   TRUE ~ 0),
+                        Dummy_B = dplyr::case_when(Partner=="B" ~ 1,
+                                                   TRUE ~ 0))
+
+        #Thanks to James Kim for distinguishable APIM nlme::gls code
+        apim_dis_A <- nlme::gls(Y_Actor ~ X_Actor + X_Partner + Dummy_A +
+                                  X_Actor*Dummy_A + X_Partner*Dummy_A,
+                                data = dataAPIM,
+                                method = "ML",
+                                correlation = corCompSymm (form=~1|ID),
+                                weights = varIdent(form=~1|Dummy_A),
+                                na.action = na.omit)
+
+        apim_dis_B <- nlme::gls(Y_Actor ~ X_Actor + X_Partner + Dummy_B +
+                                  X_Actor*Dummy_B + X_Partner*Dummy_B,
+                                data = dataAPIM,
+                                method = "ML",
+                                correlation = corCompSymm (form=~1|ID),
+                                weights = varIdent(form=~1|Dummy_B),
+                                na.action = na.omit)
+
+        sample.out <- list(apim_dis_A = apim_dis_A,
+                           apim_dis_B = apim_dis_B)
+
+        simCount <- i+1
+
+        if(output == "paramTable"){
+          sim.out <- getParamsMLM(fit = sample.out, sampCount = simCount, first = FALSE)
+        }
+
+        sim.dat <- dplyr::bind_rows(sim.dat, sim.out)
+      }
+    }
+    if(output == "paramTable"){
+      sim.dat <- sim.dat %>%
+        dplyr::mutate(pop_mod = popMod,
+                      samp_mod = sampMod,
+                      samp_n = sampSize) %>%
+        dplyr::relocate(.data$sim_num, .data$pop_mod, .data$samp_mod, .data$samp_n) %>%
+        dplyr::arrange(.data$sim_num, dplyr::desc(.data$op), .data$label)
+    }
   }
   sim.out <- list(sim.dat = sim.dat,
                   pop.params = popModData$popModParams)
